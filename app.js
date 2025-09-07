14bf87b1ec7198a1a9189535accf3f0d375ba7c9
Rajesh Jondhale <jondhaleraj2151@gmail.com>
2025-09-07 16:26:01 +0530

    phase 1 completed

---
+++ app.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const MONGO_URL = "mongodb://127.0.0.1:27017/bnbdb";
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
+//Index Route
+app.get("/listings", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+//new route
+app.get("/listings/new", wrapAsync(async (req, res) => {
+    res.render("listings/new.ejs");
+}));
+
+//show route
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+}));
+
+//create route
+app.post("/listings",
+    validateListing,
+    wrapAsync(async (req, res) => {
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    })
+);
+
+//edit route
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}));
+
+//update
+app.put("/listings/:id",
+    validateListing,
+    wrapAsync(async (req, res) => {
+        let { id } = req.params;
+        await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        res.redirect(`/listings/${id}`);
+    }));
+
+//delete route
+
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    // console.log(deletedListing);
+    res.redirect("/listings");
+}))
+
+app.all(/.*/, (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { message });
+});
+
+
+// app.get("/testListings", (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calanguate,Goa",
+//         country: "India"
+//     });
+//     res.send("working");
+//     sampleListing.save();
+// });
+
+app.listen(8080, () => {
+    console.log("Server is listening to port 8080");
+})
